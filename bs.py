dbb8be940bb77800d8d3b73794721a9002d9a467
sergiossc@gmail.com <sergiossc@gmail.com>
2019-11-26 16:10:14 -0300

    Some procedures and proporties add in this files.

--- bs.py
+++ bs.py
@@ -1,15 +1,35 @@
 import numpy as np
 import uuid
 class bs:
-    def __init__(self, number_of_antennas):
-        self.number_of_antennas = number_of_antennas
+    """
+    This is a BS device with Uniform Linear Antenna (ULA) Array. 
+    """
+    def __init__(self, number_of_tx_antennas, number_of_rx_antennas):
+        self.number_of_tx_antennas = number_of_tx_antennas
+        self.number_of_rx_antennas = number_of_rx_antennas
         self.id = uuid.uuid4()
 
-    def set_steering_vector(self):
-        self.steering_vector = np.zeros(self.number_of_antennas, 1)
+    def set_steering_vector(self, signal_transmited):
+        self.steering_vector = np.zeros(self.number_of_tx_antennas)
+
+    def set_receiving_vector(self):
+        self.receiving_vector = np.zeros(self.number_of_rx_antennas)
 
     def set_number_of_rf_chains(self, number_of_rf_chains):
         self.number_of_rf_chains = number_of_rf_chains
 
+    def set_number_of_datastreams(self, number_of_datastreams):
+        self.number_of_datastreams = number_of_rf_chains
+
     def set_baseband_precoder(self, number_of_datastreams):
         self.baseband_precoder = np.zeros((self.number_of_rf_chains, number_of_datastreams))
+
+    def set_rf_precoder(self, number_of_datastreams):
+        self.baseband_precoder = np.zeros((self.number_of_antennas, self.number_of_rf_chains))
+
+
+my_bs = bs(4, 4)
+
+print(my_bs.id)
+my_bs.set_steering_vector()
+print(my_bs.steering_vector)
---
+++ toy.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cmath
+
+p = 4
+ar_t = np.zeros(p)
+
+print (ar_t)
